ae8329dd4b16f516d43c3b6802766351879162ca
Luiz Antonio <luizantonio0125@gmail.com>
2023-02-28 17:17:21 -0300

    Creating the question Model And Preparing the questions

---
+++ app/src/main/java/br/toninho/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,15 @@
+package br.toninho.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList: ArrayList<Question> = Constants.getQuestions()
+        Log.i("QuestionsList size is", "${questionsList.size}")
+    }
+}
